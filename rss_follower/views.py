e2dde053d904364a5873ab7eb392cbbef00c757e
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-01-10 18:13:26 -0400

    Update views.py

--- rss_follower/views.py
+++ rss_follower/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import feedparser
+from .models import Author
+from .form import AuthorForm
 # Create your views here.
 
 def feed_view(request):
@@ -12,6 +14,40 @@ def feed_view(request):
 
 
 
+def author_form(request):
+    authors = Author.objects.all()
+    form = AuthorForm()
+    
+    if request.method == 'POST':
+        form =AuthorForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect("list-authors")   
+    context = {
+        'form' : form,
+        'authors': authors,
+    }  
+    return render(request, "rss_follower/author_form.html", context)     
+
+
+
+def add_link_view(request):
+    authors = {}
+    if request.method == 'POST':
+        feed_link = request.GET.get("rss_link", "")
+        author_name = request.GET.get("name", "")
+        author = Author(name=author_name, rss_link=feed_link)
+        author.save()
+
+        authors = Author.objects.all()
+    context = {"authors": authors}
+    return render(request, "rss_follower/list_authors.html", context)
+
+
+def list_authors(request):
+    authors = Author.objects.all()
+    context = {"authors": authors}
+    return render(request, "rss_follower/list_authors.html", context)
 
 
 
