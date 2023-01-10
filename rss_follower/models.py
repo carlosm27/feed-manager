7e2675dba010716ce8ec8489782d0377e06343b4
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-01-10 18:13:16 -0400

    Update models.py

--- rss_follower/models.py
+++ rss_follower/models.py
@@ -2,7 +2,5 @@ from django.db import models
 
 # Create your models here.
 class Author(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=100)
     rss_link = models.URLField(max_length=200)
-    num_entries = models.IntegerField()
-    last_entry = models.CharField(max_length=200)
