2803a97ba1bfb6d326a87086ccac7021a36d0a66
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-01-17 20:56:56 -0400

    Update urls.py

--- rss_follower/urls.py
+++ rss_follower/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("rss_feed", views.feed_view, name="rss_feed"),
+    path("rss-feed", views.feed_view, name="rss-feed"),
     path("authors", views.author_form, name='author-form'),
     path("list-authors", views.list_authors, name='list-authors'),
     
