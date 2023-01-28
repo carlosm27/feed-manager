52381a74df1cd42c009ca2e8a5759802dc41d0a9
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-01-27 22:29:02 -0400

    Create rss_info.py

---
+++ rss_follower/rss_content/rss_info.py
@@ -0,0 +1,37 @@
+import feedparser
+from datetime import date
+
+class Rss_info:
+    def __init__(self, link):
+        self.link = link
+
+        self.feed = feedparser.parse(self.link)
+        self.entries = self.feed['entries']
+
+    def author_info(self):
+        info_dict = {}
+
+        info_dict['name'] = self.entries[0]['author']
+        info_dict['number_entries'] = len(self.entries)
+        info_dict['link'] = self.link
+
+        return info_dict
+        
+
+    def last_entry_info(self):
+        last_entry_dict = {}
+
+        last_entry_dict['last_entry_title'] = self.entries[0]['title']
+        last_entry_dict['last_entry_href'] = self.entries[0]['link']
+        last_entry_dict['last_entry_date'] = self.entries[0]['published']
+
+        return last_entry_dict
+
+
+    def list_links(self, link: str):
+        feed = feedparser.parse(link)
+
+        dict = {}
+        for entry in feed['entries']:
+            dict[entry.title] = entry.links[0]['href']
+        return dict
