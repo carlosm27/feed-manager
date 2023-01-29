80418d5e3de2a197237dee9b44f82348cfeb41fc
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-01-28 21:53:27 -0400

    Update urls.py

--- rss_manager/urls.py
+++ rss_manager/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('feed/', include('rss_follower.urls'))
+    path('', include('rss_follower.urls'))
 ]
