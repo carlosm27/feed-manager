0dd8ab8b3e0e5ece61f8a5942179f228dbd9238c
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-01-31 12:11:21 -0400

    Update urls.py

--- rss_follower/urls.py
+++ rss_follower/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path("rss-feed", views.feed_view, name="rss-feed"),
     path("authors", views.author_form, name='author-form'),
     path("list-authors", views.list_authors, name='list-authors'),
-    path("author/<int:pk>", AuthorDetailView.as_view(), name = 'autor-detail'),
+    path("author/<int:pk>", AuthorDetailView.as_view(), name = 'author-detail'),
     path("all-authors", AuthorListView.as_view(), name = "all-authors")
 ]
