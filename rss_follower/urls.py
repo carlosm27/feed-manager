13687dbb7fedf0ba29f3bac31d7127e4181167cb
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-01-30 21:22:12 -0400

    Update urls.py

--- rss_follower/urls.py
+++ rss_follower/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from .views import AuthorDetailView, AuthorListView
 
 urlpatterns = [
     path("rss-feed", views.feed_view, name="rss-feed"),
     path("authors", views.author_form, name='author-form'),
     path("list-authors", views.list_authors, name='list-authors'),
-    
+    path("author/<int:pk>", AuthorDetailView.as_view(), name = 'autor-detail'),
+    path("all-authors", AuthorListView.as_view(), name = "all-authors")
 ]
