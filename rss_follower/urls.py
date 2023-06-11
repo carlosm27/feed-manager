809a4d5e34f35661e8ebc08408c4fdd81ae1680f
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-06-11 19:09:07 -0400

    Update urls.py

--- rss_follower/urls.py
+++ rss_follower/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 from .views import AuthorDetailView, AuthorListView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("rss-feed", views.feed_view, name="rss-feed"),
@@ -8,4 +10,4 @@ urlpatterns = [
     path("list-authors", views.list_authors, name='list-authors'),
     path("author/<int:pk>", AuthorDetailView.as_view(), name = 'author-detail'),
     path("all-authors", AuthorListView.as_view(), name = "all-authors")
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
